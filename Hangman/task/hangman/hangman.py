a18a81e8c0eae20a626aff4b8fb9f98a4446a17e
ginocruse <famback17@gmail.com>
2020-04-10 23:36:27 -0500

    Added a if statement and 2 new variables

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,2 +1,8 @@
 # Write your code here
-print("H A N G M A N\nThe game will be available soon.")
+print("H A N G M A N")
+game_set_word = "python"
+player_choice = input("Guess the word: > ")
+if player_choice == game_set_word:
+    print("You survived!")
+else:
+    print("You are hanged!")
